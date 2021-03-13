3fc00d1cf080d08d9b8a02ef3ba88d0d9c1d500e
ny <nouros44@gmail.com>
2021-03-13 08:20:26 +0600

    Update and rename _16_threads_advance.py to _16_threads.py

--- _01_py_core/_01_basics/_16_threads.py
+++ _01_py_core/_01_basics/_16_threads.py
@@ -1,12 +1,24 @@
 '''Threading in Python
-In this article we talk about how to use the threading module in Python.
+Threads: An entity within a process that can be scheduled aka lightweight process. A process can spawn multiple threads. 
 
-How to create and start multiple threads
-How to wait for threads to complete
-How to share data between threads
-How to use Locks to prevent race conditions
-What is a daemon thread
-How to use a Queue for thread-safe data/task processing.
++ All threads within a process share the same memory.
++ Lightweight
++ Starting a thread is faster than starting a process.
++ Great for I/O-bound tasks.
+
+- Threading is limited by GIL: Only one thread at a time.
+- No effect for CPU-bound tasks
+- Not interruptable/killable
+- Careful with race conditions. (Race condition: when two or more threads wants to access the same resource)
+
+Working: 
+- How to create and start multiple threads
+- How to wait for threads to complete
+- How to share data between threads
+- How to use Locks to prevent race conditions
+- What is a daemon thread
+- How to use a Queue for thread-safe data/task processing.
+
 Create and run threads
 You create a thread with threading.Thread(). It takes two important arguments:
 
@@ -42,9 +54,11 @@ if __name__ == "__main__":
         thread.join()
 
 '''Share data between threads
-Since threads live in the same memory space, they have access to the same (public) data. Thus, you can for example simply use a global variable to which all threads have read and write access.
+Since threads live in the same memory space, they have access to the same (public) data. Thus, you can for example simply use a global variable to which all threads have read 
+and write access.
 
-Task: Create two threads, each thread should access the current database value, modify it (in this case only increase it by 1), and write the new value back into the database value. Each thread should do this operation 10 times.'''
+Task: Create two threads, each thread should access the current database value, modify it (in this case only increase it by 1), and write the new value back into the database 
+value. Each thread should do this operation 10 times.'''
 
 from threading import Thread
 import time
@@ -89,10 +103,15 @@ if __name__ == "__main__":
 Notice that in the above example, the 2 threads should increment the value by 1, so 2 increment operations are performed. But why is the end value 1 and not 2?
 
 Race condition
-A race condition happened here. A race condition occurs when two or more threads can access shared data and they try to change it at the same time. Because the thread scheduling algorithm can swap between threads at any time, you don't know the order in which the threads will attempt to access the shared data. In our case, the first thread accesses the database_value (0) and stores it in a local copy. It then increments it (local_copy is now 1). With our time.sleep() function that just simulates some time consuming operations, the programm will swap to the second thread in the meantime. This will also retrieve the current database_value (still 0) and increment the local_copy to 1. Now both threads have a local copy with value 1, so both will write the 1 into the global database_value. This is why the end value is 1 and not 2.
+A race condition happened here. A race condition occurs when two or more threads can access shared data and they try to change it at the same time. Because the thread 
+scheduling algorithm can swap between threads at any time, you don't know the order in which the threads will attempt to access the shared data. In our case, the first thread 
+accesses the database_value (0) and stores it in a local copy. It then increments it (local_copy is now 1). With our time.sleep() function that just simulates some time 
+consuming operations, the programm will swap to the second thread in the meantime. This will also retrieve the current database_value (still 0) and increment the local_copy 
+to 1. Now both threads have a local copy with value 1, so both will write the 1 into the global database_value. This is why the end value is 1 and not 2.
 
 Avoid race conditions with Locks
-A lock (also known as mutex) is a synchronization mechanism for enforcing limits on access to a resource in an environment where there are many threads of execution. A Lock has two states: locked and unlocked. If the state is locked, it does not allow other concurrent threads to enter this code section until the state is unlocked again.
+A lock (also known as mutex) is a synchronization mechanism for enforcing limits on access to a resource in an environment where there are many threads of execution. A Lock has 
+two states: locked and unlocked. If the state is locked, it does not allow other concurrent threads to enter this code section until the state is unlocked again.
 
 Two functions are important:
 
@@ -100,7 +119,8 @@ lock.acquire() : This will lock the state and block
 lock.release() : This will unlock the state again.
 Important: You should always release the block again after it was acquired!
 
-In our example the critical code section where database values are retrieved and modified is now locked. This prevents the second thread from modyfing the global data at the same time. Not much has changed in our code. All new changes are commented in the code below.'''
+In our example the critical code section where database values are retrieved and modified is now locked. This prevents the second thread from modyfing the global data at the 
+same time. Not much has changed in our code. All new changes are commented in the code below.'''
 
 # import Lock
 from threading import Thread, Lock
@@ -147,7 +167,8 @@ if __name__ == "__main__":
 
 
 '''Use the lock as a context manager
-After lock.acquire() you should never forget to call lock.release() to unblock the code. You can also use a lock as a context manager, wich will safely lock and unlock your code. It is recommended to use a lock this way:'''
+After lock.acquire() you should never forget to call lock.release() to unblock the code. You can also use a lock as a context manager, wich will safely lock and unlock your 
+code. It is recommended to use a lock this way:'''
 
 def increase(lock):
     global database_value 
@@ -165,7 +186,8 @@ Using Queues in Python
 Queues can be used for thread-safe/process-safe data exchanges and data processing both in a multithreaded and a multiprocessing environment.
 
 The queue
-A queue is a linear data structure that follows the First In First Out (FIFO) principle. A good example is a queue of customers that are waiting in line, where the customer that came first is served first.'''
+A queue is a linear data structure that follows the First In First Out (FIFO) principle. A good example is a queue of customers that are waiting in line, where the customer
+that came first is served first.'''
 
 from queue import Queue
 
@@ -195,7 +217,12 @@ q.put(item) : Puts element at the end of the queue. By default, it blocks until 
 q.task_done() : Indicate that a formerly enqueued task is complete. For each get() you should call this after you are done with your task for this item.
 q.join() : Blocks until all items in the queue have been gotten and proccessed (task_done() has been called for each item).
 q.empty() : Return True if the queue is empty.
-The following example uses a queue to exchange numbers from 0...19. Each thread invokes the worker method. Inside the infinite loop the thread is waiting until items are available due to the blocking q.get() call. When items are available, they are processed (i.e. just printed here), and then q.task_done() tells the queue that processing is complete. In the main thread, 10 daemon threads are created. This means that they automatically die when the main thread dies, and thus the worker method and infinite loop is no longer invoked. Then the queue is filled with items and the worker method can continue with available items. At the end q.join() is necessary to block the main thread until all items have been gotten and proccessed.'''
+
+The following example uses a queue to exchange numbers from 0...19. Each thread invokes the worker method. Inside the infinite loop the thread is waiting until items are 
+available due to the blocking q.get() call. When items are available, they are processed (i.e. just printed here), and then q.task_done() tells the queue that processing is 
+complete. In the main thread, 10 daemon threads are created. This means that they automatically die when the main thread dies, and thus the worker method and infinite loop is
+no longer invoked. Then the queue is filled with items and the worker method can continue with available items. At the end q.join() is necessary to block the main thread until 
+all items have been gotten and proccessed.'''
 
 
 from threading import Thread, Lock, current_thread
@@ -238,4 +265,6 @@ if __name__ == '__main__':
 
 
 '''Daemon threads
-In the above example, daemon threads are used. Daemon threads are background threads that automatically die when the main program ends. This is why the infinite loops inside the worker methods can be exited. Without a daemon process we would have to use a signalling mechanism such as a threading.Event to stop the worker. But be careful with daemon processes: They are abruptly stopped and their resources (e.g. open files or database transactions) may not be released/completed properly.'''
+In the above example, daemon threads are used. Daemon threads are background threads that automatically die when the main program ends. This is why the infinite loops inside
+the worker methods can be exited. Without a daemon process we would have to use a signalling mechanism such as a threading.Event to stop the worker. But be careful with 
+daemon processes: They are abruptly stopped and their resources (e.g. open files or database transactions) may not be released/completed properly.'''
