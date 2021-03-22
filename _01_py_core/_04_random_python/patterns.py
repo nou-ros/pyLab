28ea3d517fbad29b02ef14bd0d8d2ab2b8e8fb49
ny <nouros44@gmail.com>
2021-03-22 12:26:10 +0600

    Update patterns.py

--- _01_py_core/_04_random_python/patterns.py
+++ _01_py_core/_04_random_python/patterns.py
@@ -137,3 +137,20 @@ for row in range(2, 6):
     start = stop
     stop += row
     current_num = stop
+ 
+'''
+*
+**
+* *
+*  *
+*****
+n = int(input("Enter the number of rows: "))
+for row in range(n):
+    for col in range(n):
+        if col == 0 or row==(n-1) or row==col:
+            print("*", end='')
+        else:
+            print(end=" ")
+    print()
+
+'''
