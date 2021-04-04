3cb559bfcffc5d0d534d2682709a99f8c2dbc0c3
ny <nouros44@gmail.com>
2021-04-04 17:20:42 +0600

    Delete _07_django_lab/_02_django_api/_00_django_drf directory

--- _07_django_lab/_02_django_api/_00_django_drf/_04_django_rest_level_two/ebooksapi/ebooks/admin.py
+++
@@ -1,6 +0,0 @@
-from django.contrib import admin
-from .models import Ebook, Review
-
-# Register your models here.
-admin.site.register(Ebook)
-admin.site.register(Review)
--- _07_django_lab/_02_django_api/_00_django_drf/_04_django_rest_level_two/readme.md
+++
@@ -1,4 +0,0 @@
-1. GenericAPIView and Mixins
-2. Concrete View Classes
-3. Permission/Auth django rest framework permission
-5. permission with foreign key
