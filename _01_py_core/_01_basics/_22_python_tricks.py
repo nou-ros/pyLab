5b02be830071ecf46014120a05c2b490c801c64b
ny <nouros44@gmail.com>
2021-04-11 10:07:18 +0600

    Update _22_python_tricks.py

--- _01_py_core/_01_basics/_22_python_tricks.py
+++ _01_py_core/_01_basics/_22_python_tricks.py
@@ -87,3 +87,83 @@ print(name)
 name = "madam"
 isPalin = name.find(name[::-1]) == 0
 print(isPalin)
+
+# Assign Multiple Variables on One Line
+x,y,z = 5, 10, 15
+print(x, y, z)
+
+# Print colored text
+print(f'\033[91mError: This is \033[96mcyan. \33[92mContinue?')
+
+# Banker's Rounding - round half towards nearest even number
+print(round(10.5))
+print(round(11.5))
+
+# Underscores in numbers
+my_account_number = 999_999_991_91
+print(my_account_number)
+
+# Open a web browser
+import webbrowser
+# webbrowser.open('https://www.google.com')
+
+# Concatenation without +
+message = "Hello world, " "this is, nouros!!"
+print(message)
+
+# Split string on multiple lines
+print("This is a really long message that I split up over "
+"multiple lines for convenience sake. "
+"The actual output is the same")
+
+"""
+# Multiline string
+print('''This is a really long message that I split up over multiple \
+    lines for convenience sake. The actual output is the same''')
+"""
+# Multiline comment
+"""
+print("Ignore")
+print("Ignore again")
+"""
+# Using id to get identity -- guarenteed to be unique for each object
+data = {"Bleach" : 379 }
+print(id(data))
+
+# replace list content
+def relpace_list(lang):
+    lang = ['new']
+
+def replace_list_content(lang):
+    lang[:] = ['new']
+
+lang = ['C++','G0', 'Python', 'JS', 'JAVA']
+print(lang)
+relpace_list(lang)
+print(lang)
+replace_list_content(lang)
+print(lang)
+
+# Copy a list with slicing
+languages = ['C++', 'Go', 'Python']
+learning = languages[:]
+print(id(languages), id(learning))
+
+# check empty list
+data = []
+# to check empty, None or zero
+print(not data)
+
+# unpack list items
+def position(x, y, z):
+    print(f'after spliting into individual items: {x} {y} {z}')
+
+pos = [5, 10, 15]
+position(*pos)
+
+# iterate backwards with reversed
+pets = ['dog', 'dog', 'cat', 'bird', 'cat', 'chicked', 'cat', 'dog']
+for pet in reversed(pets):
+    print(pet, end=" ")
+
+
